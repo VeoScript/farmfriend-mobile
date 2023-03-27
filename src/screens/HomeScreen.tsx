ff07e4f764bbd7ec6326ab490935ee71d8e64928
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-03-27 16:14:55 +0800

    feat: add settings modal display

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,6 +3,7 @@ import MainLayout from '../layouts/MainLayout'
 import tw from '../styles/tailwind'
 import { FeatherIcon } from '../utils/Icons'
 import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { useNavigate } from '../config/RootNavigation'
 import { links } from '../config/Paths'
 
 const HomeScreen = (): JSX.Element => {
@@ -12,14 +13,14 @@ const HomeScreen = (): JSX.Element => {
         <View style={tw`flex-col items-center w-full p-3`}>
           <View style={tw`relative my-1`}>
             <Image
-              style={tw`rounded-full w-[10rem] h-[10rem]`}
+              style={tw`rounded-full w-[10rem] h-[10rem] bg-olive-semi-light`}
               resizeMode="cover"
-              source={require('../assets/images/aesthetics.jpg')}
+              source={require('../assets/images/aesthetics2.jpg')}
             />
             <TouchableOpacity
               activeOpacity={0.5}
               style={tw`absolute bottom-2 right-2 z-10 rounded-full p-2 bg-white bg-opacity-50`}
-              onPress={() => console.log('Show Settings')}
+              onPress={() => console.log('Change Profile Photo')}
             >
               <FeatherIcon size={15} name="camera" color="#333333" />
             </TouchableOpacity>
@@ -32,11 +33,12 @@ const HomeScreen = (): JSX.Element => {
           </View>
         </View>
         <View style={tw`flex-row flex-wrap justify-center w-full my-2`}>
-          {links.map((module: { name: string, icon: string }, i: number) => (
+          {links.map((module: { name: string, icon: string, route: string }, i: number) => (
             <TouchableOpacity
               key={i}
               activeOpacity={0.5}
               style={tw`flex-col items-center justify-center w-[8rem] h-[8rem] m-2 p-5 rounded-xl shadow-md border border-olive-semi-light bg-olive-light`}
+              onPress={() => useNavigate(module.route)}
             >
               <FeatherIcon size={50} name={module.icon} color="#425951" />
               <Text style={tw`mt-1 font-poppins text-xs text-center text-olive`}>{ module.name }</Text>
