1fc1055a6bb5be1965958482a62bd652451b35aa
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-03-30 00:07:50 +0800

    feat: add display farmers and lgu or ngo users (#19)

--- src/screens/MajorScreens/SearchProgramsScreen.tsx
+++ src/screens/MajorScreens/SearchProgramsScreen.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import MainLayout from '../../layouts/MainLayout'
+import ProgramDetailsModal from '../../components/Modals/ProgramDetailsModal'
 import tw from '../../styles/tailwind'
 import { FeatherIcon } from '../../utils/Icons'
-import { View, Text, TextInput, ActivityIndicator, FlatList } from 'react-native'
+import { View, Text, TextInput, ActivityIndicator, FlatList, TouchableOpacity } from 'react-native'
+import { useNavigate } from '../../config/RootNavigation'
+import { useBackHandler } from '../../helpers/hooks/useBackHandler'
 import { useGetPrograms } from '../../helpers/tanstack/queries/programs'
 
 const SearchProgramsScreen = () => {
 
   const [search, setSearch] = React.useState<string>('')
+
+  const [title, setTitle] = React.useState<string>('')
+  const [description, setDescription] = React.useState<string>('')
+  const [modalVisible, setModalVisible] = React.useState<boolean>(false)
+
+  useBackHandler(() => {
+    useNavigate('HomeScreen')
+  })
 
   const {
     data: programs,
@@ -35,10 +46,10 @@ const SearchProgramsScreen = () => {
     return (
       <View style={tw`flex-1 flex-col items-center justify-center w-full my-3 `}>
         <View style={tw`flex-1 w-full max-w-xs h-full`}>
-          <Text style={tw`font-poppins text-base text-olive`}>
+          <Text style={tw`font-poppins text-sm text-olive`}>
             {search
-              ? `There is no program titled ${search}.`
-              : 'There is no created programs as of now.'
+              ? `There's no result in keyword of '${search}'.`
+              : `There's no created programs as of now.`
             }
           </Text>
         </View>
@@ -48,11 +59,19 @@ const SearchProgramsScreen = () => {
 
   const renderData = (item: any) => {
     return (
-      <View style={tw`flex-col w-full px-3 my-1`}>
-        <View style={tw`flex-col w-full p-3 rounded-xl border border-olive`}>
+      <View style={tw`flex-col w-full`}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={tw`flex-col w-full p-3 border-b border-olive-semi-light`}
+          onPress={() => {
+            setTitle(item.item.title)
+            setDescription(item.item.description)
+            setModalVisible(true)
+          }}
+        >
           <Text style={tw`my-0.5 font-poppins-bold text-sm text-olive-dark`}>{ item.item.title }</Text>
           <Text style={tw`my-0.5 font-poppins text-xs text-olive`}>{ item.item.description }</Text>
-        </View>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -72,8 +91,9 @@ const SearchProgramsScreen = () => {
           </View>
         </View>
         {isLoading
-          ? <View>
-              <Text>Loading...</Text>
+          ? <View style={tw`flex-col items-center w-full my-10`}>
+              <ActivityIndicator style={tw`pb-3`} color='#425951' size={40} />
+              <Text style={tw`font-poppins text-base`}>Loading...</Text>
             </View>
           : <FlatList
               ListEmptyComponent={listIsEmpty}
@@ -86,6 +106,12 @@ const SearchProgramsScreen = () => {
             />
         }
       </View>
+      <ProgramDetailsModal
+        title={title}
+        description={description}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
     </MainLayout>
   )
 }
--- src/screens/MajorScreens/UserLguNgoScreen.tsx
+++ src/screens/MajorScreens/UserLguNgoScreen.tsx
@@ -1,14 +1,140 @@
 import React from 'react'
 import MainLayout from '../../layouts/MainLayout'
+import UserDetailsModal from '../../components/Modals/UserDetailsModal'
 import tw from '../../styles/tailwind'
-import { Text, View } from 'react-native'
+import { FeatherIcon } from '../../utils/Icons'
+import { View, Text, TextInput, ActivityIndicator, FlatList, TouchableOpacity, Image } from 'react-native'
+import { useNavigate } from '../../config/RootNavigation'
+import { useBackHandler } from '../../helpers/hooks/useBackHandler'
+import { useGetUsers } from '../../helpers/tanstack/queries/users'
 
 const UserLguNgoScreen = () => {
-  return (
-    <MainLayout title="User LGU/NGO">
-      <View style={tw`flex-1 flex-col w-full p-3`}>
-        <Text>User LGU/NGO Screen</Text>
+
+  const [search, setSearch] = React.useState<string>('')
+
+  const [image, setImage] = React.useState<string>('')
+  const [name, setName] = React.useState<string>('')
+  const [address, setAddress] = React.useState<string>('')
+  const [contactNum, setContactNum] = React.useState<string>('')
+  const [modalVisible, setModalVisible] = React.useState<boolean>(false)
+
+  useBackHandler(() => {
+    useNavigate('HomeScreen')
+  })
+
+  const {
+    data: users,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage
+  }: any = useGetUsers('LGU_NGO', search)
+
+  const itemKeyExtractor = (item: any, index: { toString: () => any }) => {
+    return index.toString()
+  }
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage()
+    }
+  }
+
+  const renderSpinner = () => {
+    return <ActivityIndicator style={tw`pb-3`} color='#425951' size={40} />
+  }
+
+  const listIsEmpty = () => {
+    return (
+      <View style={tw`flex-1 flex-col items-center justify-center w-full my-3 `}>
+        <View style={tw`flex-1 w-full max-w-xs h-full`}>
+          <Text style={tw`font-poppins text-sm text-olive`}>
+            {search
+              ? `There's no result in keyword of '${search}'.`
+              : `There's no created users as of now.`
+            }
+          </Text>
+        </View>
       </View>
+    )
+  }
+
+  const renderData = (item: any) => {
+    return (
+      <View style={tw`flex-col w-full`}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={tw`flex-row w-full p-3 border-b border-olive-semi-light`}
+          onPress={() => {
+            setImage(item.item.image)
+            setName(item.item.first_name + ' ' + item.item.last_name)
+            setAddress(item.item.address)
+            setContactNum(item.item.contact_num)
+            setModalVisible(true)
+          }}
+        >
+          {item.item.image
+            ? <Image
+                style={tw`rounded-full w-[3rem] h-[3rem] bg-olive`}
+                resizeMode="cover"
+                source={require('../../assets/images/aesthetics2.jpg')}
+              />
+            : <View style={tw`flex-row items-center justify-center w-[3rem] h-[3rem] p-2 overflow-hidden rounded-full bg-olive`}>
+                <FeatherIcon
+                  name="user"
+                  size={20}
+                  color="#8EB6AD"
+                />
+              </View>
+          }
+          <View style={tw`flex-col ml-3`}>
+            <Text style={tw`my-0.5 font-poppins-bold text-sm text-olive-dark`}>{ item.item.first_name + ' ' + item.item.last_name }</Text>
+            <Text style={tw`my-0.5 font-poppins text-xs text-olive`}>{ item.item.address }</Text>
+            <Text style={tw`my-0.5 font-poppins text-xs text-olive`}>{ item.item.contact_num }</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  return (
+    <MainLayout title="Search LGU/NGO">
+      <View style={tw`flex-1 flex-col w-full`}>
+        <View style={tw`flex-col w-full px-3 mb-1`}>
+          <View style={tw`flex-row items-center w-full border-b border-olive`}>
+            <FeatherIcon size={20} name="search" color="#425951" />
+            <TextInput
+              style={tw`w-full ml-2 font-poppins text-sm text-olive`}
+              placeholder="Search"
+              value={search}
+              onChangeText={(value: string) => setSearch(value)}
+            />
+          </View>
+        </View>
+        {isLoading
+          ? <View style={tw`flex-col items-center w-full my-10`}>
+              <ActivityIndicator style={tw`pb-3`} color='#425951' size={40} />
+              <Text style={tw`font-poppins text-base`}>Loading...</Text>
+            </View>
+          : <FlatList
+              ListEmptyComponent={listIsEmpty}
+              data={users.pages.map((page: any) => page.users).flat()}
+              renderItem={renderData}
+              keyExtractor={itemKeyExtractor}
+              onEndReached={loadMore}
+              onEndReachedThreshold={0.3}
+              ListFooterComponent={isFetchingNextPage ? renderSpinner : null}
+            />
+        }
+      </View>
+      <UserDetailsModal
+        image={image}
+        name={name}
+        address={address}
+        contact_num={contactNum}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
     </MainLayout>
   )
 }
