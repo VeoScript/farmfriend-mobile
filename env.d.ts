b01f817ad0999e4ffcaaf78e74584c5f32099aa2
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-04 14:48:27 +0800

    feat: add check weather functionality and display (#25)
    chore: remove react-native-weather-api

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,7 @@ declare module '@env' {
   export const API_URL_DEVELOPMENT: string;
   export const API_URL_PRODUCTION: string;
   export const IMGBB_API_SECRET: string;
+  export const WEATHER_API_KEY: string;
   export const JWT_SECRET: string;
   export const EMAILJS_SERVICE_ID: string;
   export const EMAILJS_TEMPLATE_ID: string;
