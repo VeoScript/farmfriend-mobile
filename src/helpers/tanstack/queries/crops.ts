6cd553d81657ed5c8b2a6d4e9bf7c8cbd7a661c5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-04-06 13:42:27 +0800

    feat: add suggested crops dependent by weather temperature (#38)
    chore: update refetch suggested crops

--- src/helpers/tanstack/queries/crops.ts
+++ src/helpers/tanstack/queries/crops.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery } from '@tanstack/react-query'
+import { useQuery, useInfiniteQuery } from '@tanstack/react-query'
 import api from '../../../config/Axios'
 
 export const useGetCrops = (search: string) => {
@@ -11,6 +11,20 @@ export const useGetCrops = (search: string) => {
       getNextPageParam: (lastPage) => lastPage.nextId ?? false,
       onError: (error: any) => {
         console.error('ERROR CROPS', error.response.data)
+      }
+    }
+  )
+}
+
+export const useGetSuggestedCrops = (search: string) => {
+  return useQuery(['suggestedCrops', search],
+    async () => {
+      const suggestedCrops = await api.get(`/api/suggested-crops?search=${search}`)
+      return suggestedCrops.data
+    },
+    {
+      onError: (error: any) => {
+        console.error('ERROR SUGGESTED CROPS', error.response.data)
       }
     }
   )
